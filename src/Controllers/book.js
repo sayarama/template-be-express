649bdbc1b52ff708aae7fd8ec8a5612904d028a3
Aulia Ramadhan <auliarama965@gmail.com>
2024-03-07 18:00:43 +0700

    Update BE

--- src/Controllers/book.js
+++ src/Controllers/book.js
@@ -1,5 +1,7 @@
 const modelBuku = require("../Models/book")
 
+const {Validator} = require("node-input-validator");
+
 const bookController = {
     _getAllBuku: async (req, res) => {
         try {
@@ -17,7 +19,60 @@ const bookController = {
                 data: []
             });
         }
-    }
+    },
+    _getDetailBuku: async (req, res) => {
+        try {
+            const request = await modelBuku.getDetailBuku()
+
+            res.status(200).json({
+                status: true,
+                message: "Get data success",
+                data: request,
+            })
+        } catch (error) {
+            res.status(502).json({
+                status: false,
+                message: "Something Wrong In Our Server",
+                data: []
+            });
+        }
+    },
+    _getNewBuku: async (req, res) => {
+        try{
+            const request = await modelBuku.getNewBuku()
+
+            res.status(200).json({
+                status: true,
+                message: "Get data success",
+                data: request,
+            })
+        } catch (error) {
+            res.status(502).json({
+                status: false,
+                message: "Something Wrong In Our Server",
+                data: [],
+            })
+        }
+    },
+    _inputValidation: async (req, res, next) => {
+        const schema = new Validator(req.body, {
+            name: "required|minLength:5|maxLength:100",
+            author: "required|minLength:5|maxLength:50",
+        });
+
+        schema.check().then((matched) => {
+            if (!matched) {
+                res.status(422).send({
+                    status: false,
+                    message: schema.errors,
+                    data:null,
+                });
+            } else {
+                next();
+            }
+        })
+    },
+    _addBuku
 }
 
 module.exports = bookController;
--- src/Models/book.js
+++ src/Models/book.js
@@ -6,6 +6,22 @@ const modelBuku = {
 
         return request;
     },
+    getDetailBuku: async () => {
+        const request = await database`SELECT * FROM book WHERE id = ${id}`;
+
+        return request;
+    },
+    getNewBuku: async () => {
+        const request = await database`SELECT * FROM book ORDER BY id DESC LIMIT 1`;
+
+        return request;
+    },
+    addBuku: async (payload) => {
+        const { name, author, price, release } = payload;
+        const request = await database`INSERT INTO book (name, author, price, release) values (${name}, ${author}, ${price}, ${release}) RETURNING id`;
+
+        return request;
+    }
 }
 
 module.exports = modelBuku;
